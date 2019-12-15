ac4557ca0f4da1762fd5853a7fdae2685c46ec3e
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-12-15 22:58:37 +0900

    create AddWordViewController.swift

--- apple3/AddWordViewController.swift
+++ apple3/AddWordViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class AddWordViewController: UIViewController {
 
+    @IBOutlet weak var MeanTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 枠のカラー
+        MeanTextView.layer.borderColor = UIColor.black.cgColor
+        // 枠の幅
+        MeanTextView.layer.borderWidth = 0.5
+        
 
         // Do any additional setup after loading the view.
     }
     
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // テキスト入力部以外をタッチするとキーボードを格納
+        self.view.endEditing(true)
+    }
+    // 語の意味を入力するフィールド
+    @IBAction func wordTextField(_ sender: Any) {
+    }
+    
+    @IBAction func addWordButton(_ sender: Any) {
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextView = storyboard.instantiateViewController(withIdentifier: "Secondpage") as! SecondViewController
+        self.present(nextView, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
