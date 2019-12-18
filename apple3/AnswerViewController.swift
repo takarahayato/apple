fe57d40752cfc905be54a0f705c8a4cb688ab686
matsuda-kazuhide <e185756@ie.u-ryukyu.ac.jp>
2019-12-19 01:11:56 +0900

    AnswerViewController Change

--- apple3/AnswerViewController.swift
+++ apple3/AnswerViewController.swift
@@ -9,12 +9,131 @@
 import UIKit
 
 class AnswerViewController: UIViewController {
+    
+    @IBOutlet weak var Question: UILabel!
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    
+    @IBOutlet weak var Answer: UILabel!
+    
+    @IBOutlet weak var NextButton: UIButton!
+    
+    
+    var RandomNumber :Int!
+    var Judgment : DarwinBoolean!
+    var RandomFour = [0,1,2,3]
+    var Choices = [Int]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        RandomQuestions()
+        
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    func RandomQuestions(){
+        Hide()
+        Choices = [Int]()
+        Choices.append(Int(arc4random()) % words.count)
+        
+        for _ in 0 ... 2{
+            Loop()
+        }
+        RandomFour.shuffle()
+        
+        Question.text = words[Choices[0]][0]
+        Button1.setTitle(words[Choices[RandomFour[0]]][1], for: UIControl.State())
+        Button2.setTitle(words[Choices[RandomFour[1]]][1], for: UIControl.State())
+        Button3.setTitle(words[Choices[RandomFour[2]]][1], for: UIControl.State())
+        Button4.setTitle(words[Choices[RandomFour[3]]][1], for: UIControl.State())
+    }
+    
+    
+    func Loop(){
+        Judgment = true
+        RandomNumber = Int(arc4random()) % words.count
+        for i in stride(from: 0, to: Choices.count, by: 1){
+            if (RandomNumber == Choices[i]){
+                Loop()
+                Judgment = false
+            }
+        }
+        if(Judgment == true){
+            Choices.append(RandomNumber)
+        }
+       
+    }
+    
+    func Hide(){
+        Answer.isHidden = true
+        NextButton.isHidden = true
+    }
+    func UnHide(){
+        Answer.isHidden = false
+        NextButton.isHidden = false
+    }
+    
+
+    
+    @IBAction func Button1Action(_ sender: Any) {
+        if(words[Choices[RandomFour[0]]][0] == words[Choices[0]][0]){
+            Answer.text = "正解！"
+        }
+        else{
+            Answer.text = "残念正解は\(words[Choices[0]][1])"
+        }
+        UnHide()
+    }
+    
+    @IBAction func Button2Action(_ sender: Any) {
+        if(words[Choices[RandomFour[1]]][0] == words[Choices[0]][0]){
+            Answer.text = "正解！"
+        }
+        else{
+            
+            Answer.text = "残念正解は\(words[Choices[0]][1])"
+        }
+        UnHide()
+    }
+    
+    
+    @IBAction func Button3Action(_ sender: Any) {
+        if(words[Choices[RandomFour[2]]][0] == words[Choices[0]][0]){
+            Answer.text = "正解！"
+        }
+        else{
+            Answer.text = "残念正解は\(words[Choices[0]][1])"
+        }
+        UnHide()
+    }
+    
+    @IBAction func Button4Action(_ sender: Any) {
+        if(words[Choices[RandomFour[3]]][0] == words[Choices[0]][0]){
+            Answer.text = "正解！"
+        }
+        else{
+            Answer.text = "残念正解は\(words[Choices[0]][1])"
+        }
+        UnHide()
+    }
+    
+    
+    
+    @IBAction func NextButtonAction(_ sender: Any) {
+        RandomQuestions()
+    }
+    
+    
+    
+    
+    
     // backボタン押下時の処理
     @IBAction func goback(_ sender: Any) {
        
